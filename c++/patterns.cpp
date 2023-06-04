92cceb414f636e21d5c04172e5eccacb1edb49b5
80pi <gopivenkataajay5771@gmail.com>
2023-06-04 19:35:50 +0530

    learning day3

---
+++ c++/patterns.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // prime code
+    // int n,i=2,prime=0;
+    // cin >> n;
+    // while(i<n){
+    //     if(n%i==0){
+    //         cout<<"not prime";
+    //         prime=1;
+    //         break;
+    //     }
+    //     i++;
+    // }
+    // if(prime==0){
+    // cout<<"given num is prime";
+    // }
+    
+// -------------------------------------
+    // star pattern of stars
+    // int n,i=0;
+    // cin >> n;
+    // while(i<n){
+    //     int j=0;
+    //     while(j<n){
+    //         cout<<"*";
+    //         j++;
+    //     }
+    //     cout<<"\n";
+    //     i++;
+    // }
+
+    // output
+//    
+// ------------------------------------
+    
+    // pattern-2 111\n222\n333 based on n numbers
+    // int n,i=1;
+    // cin>>n;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<i;
+    //         j++;    
+    //     }
+    //     i++;
+    //     cout << "\n";
+    // }
+//    output
+// 8
+// 11111111
+// 22222222
+// 33333333
+// 44444444
+// 55555555
+// 66666666
+// 77777777
+// 88888888
+// ---------------------------------------------------
+string tt = "a"*5;
+cout<<tt;
+
+
+}
